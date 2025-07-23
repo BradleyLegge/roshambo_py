6ad6a32ab7717cd2990149680dce94ab6a0deebc
BradleyLegge <bmlegge12@gmail.com>
2025-07-23 15:23:50 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+
+def user_input(message):
+    while True:
+        try:
+            u_input = int(input(message))
+        except ValueError:
+            print("Not a valid number! Try again!")
+            continue
+        else:
+            return u_input
+        
+
+def main():
+    print("Winning rules of the game ROCK PAPER SCISSORS are:")
+    print("Rock vs Paper -> Paper wins")
+    print("Rock vs Scissors -> Rock wins")
+    print("Paper vs Scissors -> Scissors wins\n")
+
+    print("Enter your choice")
+    print("1 - Rock")
+    print("2 - Paper")
+    print("3 - Scissors")
+    users_input = user_input("Enter your choice: ")
+    print(f"Your choic is: {users_input}")
+
+if __name__ == "__main__":
+    main()
