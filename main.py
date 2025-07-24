ed106d35a52d6757f3e47134185c8b129e5ee02b
BradleyLegge <bmlegge12@gmail.com>
2025-07-24 15:20:02 -0400

    finished project

--- main.py
+++ main.py
@@ -6,62 +6,68 @@ def get_instructions():
     print("Rock vs Scissors -> Rock wins")
     print("Paper vs Scissors -> Scissors wins\n")
 
-
-# def get_user_input(message):
-#     while True:
-#         try:
-#             poop = int(input(message))
-#         except ValueError:
-#             print("Not a valid number! Try again!")
-#             continue
-#         else:
-#             return poop
-
-def get_user_choice(choice):
-    match choice:
-        case "1":
-            return "Rock"
-        case "2":
-            return "Paper"
-        case "3":
-            return "Scissors"
-        case _:
-            "Error"
-
-def get_computer_choice(users_input):
-    comp_choice = str(random.randint(1,1))
-    print(users_input)
-    print(type(users_input))
-    print(comp_choice)
-    print(type(comp_choice))
-   
-    while(users_input == comp_choice):
-        print("True")
-        comp_choice = str(random.randint(1, 3))
-        print("Trying again")
-        
-
-        
-
-def main():
-    get_instructions()
-
     print("Enter your choice")
     print("1 - Rock")
     print("2 - Paper")
     print("3 - Scissors")
-    # users_input = get_user_input("Enter your choice: ")   May need this line later!
-    users_input = input("Enter your choice: ")
-    users_choice = str(get_user_choice(users_input))
-    print(type(users_input))
-    print(type(users_choice))
+
+def get_choice(choice):
+    if( choice == 1):
+        return "Rock"
+    elif( choice == 2):
+        return "Paper"
+    elif( choice == 3):
+        return "Scissors"
+    else:
+        return "Invalid choice"
+
+def get_computer_choice():
+    comp_choice = random.randint(1,3)
+    return comp_choice
+
+def get_winner(user, computer):
+    if(user == computer):
+        return "<== It's a tie! ==>"
+    elif (user == "Rock" and computer == "Scissors") or (user == "Paper" and computer == "Rock") or (user == "Scissors" and computer == "Paper"):
+        return "<== You win! ==>"
+    elif (computer == "Rock" and user == "Scissors") or (computer == "Paper" and user == "Rock") or (computer == "Scissors" and user == "Paper"):
+        return "<== Computer wins! ==>"
     
-    get_computer_choice(users_input)
+def play_again(answer):
+    if(answer.lower() == "y"):
+        main()
+    elif(answer.lower() =="n"):
+        exit
+    else:
+        print("Invalid input. Please enter 'Y' or 'N'.")
+        answer = input("Do you want to play again? (Y/N): ")
+        play_again(answer)
+
+def main():
+    get_instructions()
+
+    # Update: Would like to move this while loop into its own validation function
+    while True: 
+        try: 
+            user_input = int(input("Enter your choice (1-3): "))
+            while 1 < user_input > 3:
+                user_input = int(input("Please choose a number between 1 and 3: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 3: ") 
+
+    users_choice = get_choice(user_input)
     print(f"Your choice is: {users_choice}")
-    # print("Now it's Computer's Turn. . .")
-    # computer_choice = get_computer_choice(users_choice)
-    # print(f"Computer choice is: {computer_choice}")
-    # # print(f"{user_choice} vs {computer_choice}")
+
+    print("Now it's Computer's Turn. . .")
+    computer_input = get_computer_choice()
+    computer_choice = get_choice(computer_input)
+    print(f"Computer choice is: {computer_choice}")
+
+    print(f"{users_choice} vs {computer_choice}")
+    print(get_winner(users_choice, computer_choice))
+    answer = input("Do you want to play again? (Y/N) ")
+    play_again(answer)
 
 if __name__ == "__main__":
     main()
