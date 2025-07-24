d2fccc117654dbb7031e15d9affbaf7bd9bcf2b1
Brad <bmlegge12.gmail.com>
2025-07-24 09:29:16 -0400

    added feature

--- main.py
+++ main.py
@@ -1,54 +1,67 @@
 import random
 
-def get_user_input(message):
-    while True:
-        try:
-            poop = int(input(message))
-        except ValueError:
-            print("Not a valid number! Try again!")
-            continue
-        else:
-            return poop
-
-def get_user_choice(choice):
-    match choice:
-        case 1:
-            return "Rock"
-        case 2:
-            return "Paper"
-        case 3:
-            return "Scissors"
-        case _:
-            "Error"
-
-def get_computer_choice(users_input):
-    comp_choice = random.randint(1,3)
-    print(users_input)
-    print(comp_choice)
-    while(users_input == comp_choice):
-        comp_choice = random.randint(1,3)
-        print(comp_choice)
-        
-
-        
-
-def main():
+def get_instructions():
     print("Winning rules of the game ROCK PAPER SCISSORS are:")
     print("Rock vs Paper -> Paper wins")
     print("Rock vs Scissors -> Rock wins")
     print("Paper vs Scissors -> Scissors wins\n")
 
+
+# def get_user_input(message):
+#     while True:
+#         try:
+#             poop = int(input(message))
+#         except ValueError:
+#             print("Not a valid number! Try again!")
+#             continue
+#         else:
+#             return poop
+
+def get_user_choice(choice):
+    match choice:
+        case "1":
+            return "Rock"
+        case "2":
+            return "Paper"
+        case "3":
+            return "Scissors"
+        case _:
+            "Error"
+
+def get_computer_choice(users_input):
+    comp_choice = str(random.randint(1,1))
+    print(users_input)
+    print(type(users_input))
+    print(comp_choice)
+    print(type(comp_choice))
+   
+    while(users_input == comp_choice):
+        print("True")
+        comp_choice = str(random.randint(1, 3))
+        print("Trying again")
+        
+
+        
+
+def main():
+    get_instructions()
+
     print("Enter your choice")
     print("1 - Rock")
     print("2 - Paper")
     print("3 - Scissors")
-    users_input = get_user_input("Enter your choice: ")
+    # users_input = get_user_input("Enter your choice: ")   May need this line later!
+    users_input = input("Enter your choice: ")
     users_choice = str(get_user_choice(users_input))
+    print(type(users_input))
+    print(type(users_choice))
+    
+    get_computer_choice(users_input)
     print(f"Your choice is: {users_choice}")
-    print("Now it's Computer's Turn. . .")
-    computer_choice = get_computer_choice(users_choice)
-    print(f"Computer choice is: {computer_choice}")
-    # print(f"{user_choice} vs {computer_choice}")
+    # print("Now it's Computer's Turn. . .")
+    # computer_choice = get_computer_choice(users_choice)
+    # print(f"Computer choice is: {computer_choice}")
+    # # print(f"{user_choice} vs {computer_choice}")
 
 if __name__ == "__main__":
     main()
